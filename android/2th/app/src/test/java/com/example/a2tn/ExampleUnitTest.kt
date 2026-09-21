9aab2f37ba4578544aac3c26c78230e23c5e0a58
20240643 <whtjdals0434@gmail.com>
2026-09-21 13:41:27 +0900

    3주차 자료형추가

--- android/2th/app/src/test/java/com/example/a2tn/ExampleUnitTest.kt
+++ android/2th/app/src/test/java/com/example/a2tn/ExampleUnitTest.kt
@@ -1,7 +1,7 @@
 package com.example.a2tn
 
+import androidx.core.graphics.component4
 import org.junit.Test
-
 import org.junit.Assert.*
 
 /**
@@ -13,5 +13,47 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+
+        val myName = "조성민"
+        val age: Int = 27
+
+        println("코틀린: 불변 변수 val 나의 이름은 $myName, 나의 나이 : $age")
+
+        var numOne = 1
+        var numTwo = 3000000000
+        var myByte : Byte = 1
+        var myInt : Int = 20
+        var myLong = 25L
+
+        println("numone : $numOne\n numtwo : $numTwo\n mybyte : $myByte\n mtint : $myInt\n mylong $myLong\n")
+
+        var myFloat: Float = 30.2F
+        var myDouble: Double = 35.4
+
+        println("코틀린 : 실수 자료형, Float : $myFloat")
+        println("코틀린 : 실수 자료형, Double : $myDouble")
+
+        var myBoolean : Boolean = true
+        println("코틀린 : 부울린 자료형, Boolean : $myBoolean\n")
+
+        var myChar1: Char = 'K'
+        var myChar2: Char = 'o'
+        var myChar3: Char = 't'
+        var myChar4: Char = 'l'
+        var myChar5: Char = 'i'
+        var myChar6: Char = 'n'
+
+        println("코틀린 : 문자 자료형, Char : $myChar1$myChar2$myChar3$myChar4$myChar5$myChar6\n")
+
+
+        var myString1 : String = "Kotlin\n"
+        var myString2 : String = "Java"
+
+        println("String : $myString1")
+        println("String : $myString2")
+
+        var myArray : IntArray = intArrayOf(1,2,3,4,5)
+        println("배열의3번째 값 : " + myArray[2])
+
     }
 }
